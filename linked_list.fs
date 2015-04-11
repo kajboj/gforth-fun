cb4df2bf427b49a40d36c786e00441ca3492b57a
Kajetan Bojko <kai@shutl.co.uk>
2015-04-11 13:44:02 +0100

    allocation of the new node

--- linked_list.fs
+++ linked_list.fs
@@ -1,29 +1,45 @@
-S" linked list file" type cr
-
 2 Constant LIST_CONS_SIZE
-2 Constant LIST_METADATA_SIZE
+3 Constant LIST_METADATA_SIZE
+1 Constant LIST_COUNT_OFFSET
+2 Constant LIST_HEAD_OFFSET
+
+\ list metadata:
+\   remaining number of nodes
+\   current number of allocated nodes
+\   head of the list
 
 : nilify-cell ( addr - )
-              \ input:
-              \   address of the memory cell to be set to nil
   nil swap ! ;
+
+: zero-cell ( addr - )
+  nilify-cell ;
 
 : list-memory-size ( n -- n )
   LIST_CONS_SIZE * LIST_METADATA_SIZE + cells ;
 
-: list-reserve-memory ( addr n -- addr )
-                      \ input:
-                      \   start address
-                      \   number of nodes
-                      \ output:
-                      \   address of reserved address
-  tuck                    \ n addr n
-  list-memory-size allot  \ n addr
-  swap over !             \ addr
-  dup cell+ nilify-cell ; \ second cell points to the first cell of the list
+: list-counter ( addr -- addr1 )
+  LIST_COUNT_OFFSET cells + ;
 
-: list-head ( addr -- u )
-  cell+ @ ;
+: list-get ( addr -- addr1 )
+  LIST_HEAD_OFFSET cells + ;
+
+: list-init ( addr n -- addr )
+            \ input:
+            \   start address
+            \   number of nodes
+            \ output:
+            \   address of reserved address
+  tuck                       \ n addr n
+  list-memory-size allot     \ n addr
+  tuck !                     \ addr
+  dup list-counter zero-cell \ node counter
+  dup list-get nilify-cell ; \ head of the list
+
+: list-head ( addr -- addr1 )
+  list-get @ ;
+
+: list-node-count ( addr -- u )
+  list-counter @ ;
 
 : list-remaining-space ( addr -- u )
   @ ;
@@ -31,5 +47,36 @@ S" linked list file" type cr
 : list-empty? ( addr -- f )
   list-head nil = ;
 
+: list-full? ( addr -- f )
+  list-remaining-space 0 = ;
+
 : list-dump ( addr n -- )
-  list-memory-size dump ;
+  list-memory-size dump cr ;
+
+: list-first-empty ( addr -- addr1 )
+  dup list-node-count LIST_CONS_SIZE *
+  LIST_METADATA_SIZE + cells + ;
+
+: list-init-cons ( n addr -- )
+  tuck ! cell+ nilify-cell ;
+
+: list-dec-remaining-space ( addr -- )
+  dup @ 1- swap ! ;
+
+: list-inc-counter ( addr -- )
+  list-counter dup @ 1+ swap ! ;
+
+: list-new ( addr n -- addr )
+           \ input:
+           \   address of the list
+           \   payload of the new node
+           \ output:
+           \   address of the new allocated node
+  over                         \ addr n addr
+  list-first-empty             \ addr n addr1
+  tuck                         \ addr addr1 n addr1
+  list-init-cons               \ addr addr1
+  swap                         \ addr1 addr
+  dup                          \ addr1 addr addr
+  list-dec-remaining-space     \ addr1 addr
+  list-inc-counter ;           \ addr1
--- linked_list_test.fs
+++ linked_list_test.fs
@@ -3,13 +3,33 @@ require linked_list.fs
 
 4 Constant LIST_SIZE
 
-here LIST_SIZE list-reserve-memory
-dup LIST_SIZE list-dump
+here LIST_SIZE list-init
 
 dup list-remaining-space LIST_SIZE =
-S" list should have initially all node space available" assert
+S" list has initially all node space available" assert
 
 dup list-empty? true =
-S" list should be initially empty" assert
+S" list is initially empty" assert
+
+dup list-full? false =
+S" list is not full" assert
+
+\ after allocating new node
+
+dup 13 list-new over
+dup LIST_SIZE list-dump
+
+dup list-empty? true =
+S" list is empty" assert
+
+dup list-full? false =
+S" list is not full" assert
+
+dup list-remaining-space 3 =
+S" available space dropped by 1" assert
+
+\ after inserting new node
+
+\ S" added node is at the head" assert
 
 .s cr
